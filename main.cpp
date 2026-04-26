6740c452b5d821b159589937a9fcf61fcc8192bc
ilaysmith <ilaysmith13@yandex.ru>
2026-04-26 22:42:07 +0300

    commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <rtl-sdr.h>
+#include <fstream> // Для работы с файлами
+#include "sdr-rtl/RTL_SDR.h"
+
+
+int main() {
+
+    RTL_SDR device;
+
+    device.connectionSDR();
+    device.setParameters();
+    device.readSignal();
+    device.stopRecording();
+
+    
+    return 0;
+}
+
+
+
+// int result = rtlsdr_read_sync(dev, buffer.data(), buffer.size(), &n_read);
+// dev - указатель на устройство
+// buffer.data() - указатель на начало выделенной памяти
+// buffer.size() - количество байт, которое хотим прочитать
+// &n_read - адрес переменной, куда запишется реальное число количества прочитанных бит
+// Функция блокирует выполнение программы (ждёт).
+// Ждёт, пока с USB накопится нужное количество байт
+// Копирует данные из внутреннего буфера драйвера в buffer
+// Возвращает управления программы
+
+// rtlsdr_close() освобождает usb-интерфейс, закрывает соединение, высвобождает всю память, выделенную внутри rtlsdr_open()
+// Без закрытия приёмник может остаться в захваченном состоянии
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sdr-rtl/readSignal.cpp
@@ -0,0 +1,44 @@
+#include "../sdr-rtl/RTL_SDR.h"
+
+int RTL_SDR::readSignal() {
+
+    rtlsdr_reset_buffer(
+            dev); // сброс usb буфера - чтобы не вернуть старые данные до настройки частоты, так как приемник работает непрерывно с момента открытия
+
+    std::cout << "Чтение 16384 байт данных..." << std::endl;
+
+    // Запись данных в файл
+    std::ofstream outfile("../../iq_recording.bin", std::ios::binary);
+    if (!outfile) {
+        std::cerr << "Ошибка создания файла!" << std::endl;
+        rtlsdr_close(dev);
+        return 1;
+    }
+
+    std::cout << "Начало записи в файл iq_recording.bin. Для выхода нажать на пиктограмму прекращения работы"
+              << std::endl;
+
+    std::vector<uint8_t> buffer(
+            16384); // Динамический массив из 16384 элементов. Все элементы = 0. 16384 размер буфера в байтах
+    int n_read = 0; // здесь будет реальное количество бит
+
+    // Читаем и пишем
+    // Запись 10 секунд данных
+    int total_seconds = 10;
+    long long total_samples = sample_rate * total_seconds;
+    long long samples_read = 0;
+
+    while (samples_read < total_samples) {
+        int result = rtlsdr_read_sync(dev, buffer.data(), buffer.size(), &n_read);
+        if (result >= 0 && n_read > 0) {
+            outfile.write(reinterpret_cast<const char *>(buffer.data()), n_read);
+            std::cout << "Успешно прочитано и записано " << n_read << " байт." << std::endl;
+            samples_read += n_read / 2; // Делим на 2, т.к. каждый сэмпл — 2 байта (I+Q)
+        } else {
+            std::cerr << "Ошибка чтения данных." << std::endl;
+            break;
+        }
+    }
+    outfile.close();
+    return 0;
+}
